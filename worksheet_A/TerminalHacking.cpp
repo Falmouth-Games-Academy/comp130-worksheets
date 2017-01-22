8219218105db0b48c3aa93ff4df0f603847fd468
jack-maber <jack-maber@live.co.uk>
2017-01-22 18:15:44 +0000

    Reformatted code and added new comments

--- worksheet_A/TerminalHacking.cpp
+++ worksheet_A/TerminalHacking.cpp
@@ -4,10 +4,15 @@
 #include "stdafx.h"
 #include "WordList.h"
 
+
 const int wordlength = 5;
 const int numberOfWords = 15;
+int lives = 4;
+bool correctguess;
+int LikenessScore = 0;
+std::string input;
 
-
+// Compares the guessed word to the secret word, then returns the likeness
 int getlikeness(std::string guessedWord, std::string secretWord)
 {
 	int i;
@@ -59,50 +64,49 @@ int main()
 
 	
 	// TODO: implement the rest of the game
-	bool correctguess;
-	int Likeness = 0;
-	std::string input;
-
-	int lives = 4;
-
+	
 	//Only runs loop while the player has lives left
-	while (lives >= 0)
+	while (lives > 0)
 	{
 		correctguess = false;
 		while (correctguess == false)
 		
 		{	
 			// Allows the player to enter an anwser
-			std::cout << "Please Enter Your Guess\n";
+			std::cout << "Please Enter Your Guess in CAPS\n";
+			
 			std::cin >> input;
 
 			// Checks all of the words in the list against the player's guess
 			for each (std::string word in options)
 			
-			{
-				// Checks that the player's word is actually in the list
-				if (input == word)
-					correctguess = true;
-					
-			}
 		
+			{	// Checks that the player's word is actually in the list
+				if (input == word)
+				{
+					correctguess = true;
+				}
+				
+			}
 		}
 
 		// Gets the likeness of value of the player's guess against the secret word, by calling the created function
-		Likeness = getlikeness(input, secret);
-		std::cout << "Likeness score was = " << Likeness << "/" << wordlength << std::endl;
+		LikenessScore = getlikeness(input, secret);
+		std::cout << "Likeness score was = " << LikenessScore << "/" << wordlength << std::endl;
 		
 		// Checks if the inputted guess was correct
-		if (Likeness == wordlength)
+		if (LikenessScore == wordlength)
 		{
 			std::cout << "You Win!" << std::endl;
 			return 0;
 		}
+		
+		
 		else
 		{
 			// Removes a life if the correct anwser is not given by the player
 			lives--;
-			std::cout << "Incorrect Guesses = " << lives << std::endl;
+			std::cout << "Incorrect Guesses left = " << lives << "/4\n";
 		}
 	}
 	
