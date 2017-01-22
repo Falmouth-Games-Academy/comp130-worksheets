69ff3fa6569298c61f1cddcff22a6cf87d44a118
Jonny Marx <jonnymarx98@gmail.com>
2017-01-22 11:14:11 +0000

    Finished rest of game

--- worksheet_A/TerminalHacking.cpp
+++ worksheet_A/TerminalHacking.cpp
@@ -7,6 +7,34 @@
 const int wordLength = 5;
 const int numberOfWords = 15;
 
+// Returns number of matching letters
+int getLikeness(std::string guessedWord, std::string secretWord)
+{
+	int result = 0;
+
+	for (int i = 0; i < secretWord.length(); i += 1) {
+
+		if (secretWord.at(i) == guessedWord.at(i)) {
+			result += 1;
+		}
+	}
+
+	return result;
+}
+
+// Checks to see if word is in the list
+bool wordInList(std::string guess, std::set<std::string> options)
+{
+	for each (std::string word in options) {
+
+		if (guess == word) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
 int main()
 {
 	// Seed the random number generator with the current time,
@@ -40,7 +68,97 @@ int main()
 	}
 
 	// TODO: implement the rest of the game
+	
+	using namespace std;
 
-    return 0;
+	int lives = 4;
+	string guess;
+
+	cout << endl << "Welcome to ROBCO Industries (TM) Termlink" << endl << "Password Required";
+
+	// Main loop
+	while (lives > 0) 
+			{
+				// Tells player how many attempts they have left
+				if (lives > 1)
+				{
+					cout << endl << "Attempts Remaining: " << lives << endl << endl;
+					cin >> guess;
+					lives -= 1;
+				}
+				else 
+				{
+					cout << endl << "Attempts Remaining: " << lives << endl
+					<< "I Have a bad feeling about this" << endl << endl;
+					cin >> guess;
+					lives -= 1;
+				}
+
+				// Checks if word is in list
+				if (wordInList(guess, options)) 
+				{									
+					// Checks if word is the secret word
+					if (guess == secret) 
+					{
+						cout << "Password Accepted!" << endl << "The force is strong with you" << endl;
+						lives = 0;
+					}
+
+					else if (lives == 0) 
+					{
+						cout << "0 attempts remaining, access denied" << endl << "I find your lack of skill disturbing" << endl << 
+							"           _.-'~~~~~~`-._ " << endl <<
+							"          /      ||      \ " << endl <<
+							"         /       ||       \ " << endl <<
+							"        |        ||        | " << endl <<
+							"        | _______||_______ | " << endl <<
+							"        |/ ----- \/ ----- \| " << endl <<
+							"       /  (     )  (     )  \ " << endl <<
+							"      / \  ----- () -----  / \ " << endl <<
+							"     /   \      /||\      /   \ " << endl <<
+							"    /     \    /||||\    /     \ " << endl <<
+							"   /       \  /||||||\  /        " << endl <<
+							"  /_        \o========o/        _\ " << endl <<
+							"    `--...__|`-._  _.-'|__...--'" << endl <<
+							"            |    `'    |             " << endl;
+					}
+					else 
+					{
+						cout << "Entry denied" << endl << "Likeness = " << getLikeness(guess, secret);
+					}
+
+					if (getLikeness(guess, secret) == 0)
+					{
+						if (lives == 3)
+						{
+							cout << endl << "Once you start down the dark path, forever will it dominate your destiny";
+						}
+
+						if (lives == 2)
+						{
+							cout << endl << "Patience you must have my yong padawan";
+						}
+						
+					}
+
+					if (getLikeness(guess, secret) == 2)
+					{
+						cout << endl << "Powerful you have become, the dark side I sense in you";
+					}
+
+					if (getLikeness(guess, secret) == 1)
+					{
+						cout << endl << "Do or do not. There is no try";
+					}
+
+				}
+				else
+				{
+					cout << endl << "ERROR 57UP1D: word not in list   NOTE: the terminal is CASE sensitive" << endl ;
+				}
+			}
+
+	return 0;
 }
+
 
