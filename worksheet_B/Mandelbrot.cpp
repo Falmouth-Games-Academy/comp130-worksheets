ffc19091d4a90e179d20515be96c873c474023d0
lstent <lucstent@gmail.com>
2017-01-31 22:04:40 +0000

    Finished worksheet B

--- worksheet_B/Mandelbrot.cpp
+++ worksheet_B/Mandelbrot.cpp
@@ -1,6 +1,3 @@
-// Mandelbrot.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 
 int main()
@@ -15,37 +12,75 @@ int main()
 	const double minX = -2, maxX = 1, minY = -1.5, maxY = 1.5;
 
 	// Generate the image
-	for (int pixelY = 0; pixelY < image.height(); pixelY++)
+	for (int pixelY = 0; pixelY < 800.0; pixelY++)
 	{
 		// Maps the y coordinate into the range minY to maxY
-		double y0 = ((pixelY / image.height()) * (maxY - minY) + minY);
+		double y0 = ((pixelY / 800.0) * (maxY - minY) + minY);
 
-		for (int pixelX = 0; pixelX < image.width(); pixelX++)
+		for (int pixelX = 0; pixelX < 800.0; pixelX++)
 		{
 			// Maps the x coordinate into the range minX to maxX
-			double x0 = ((pixelX / image.width()) * (maxX - minX) + minX);
+			double x0 = ((pixelX / 800.0) * (maxX - minX) + minX);
 
-			// TODO: implement the algorithm to colour a single pixel (x0, y0) of the Mandelbrot set fractal
-			// The code below simply fills the screen with random pixels
+			// The algorithm to colour a single pixel (x0, y0) of the Mandelbrot set fractal
+			double x = 0;
+			double y = 0;
+			int iteration = 0;
+			int MaxIteration = 200;
+			while ((x * x) + (y * y) < 4 && iteration < MaxIteration)
+			{
+				double xtemp = (x * x) - (y * y) + x0;
+				y = 2 * x * y + y0;
+				x = xtemp;
+				iteration++;
+			}
 
+			//Converts the colours from HSV to RGB
+			int Red = 0;
+			int Green = 0;
+			int Blue = 0;
+
+			if (iteration <= 60)
+			{
+				Red = 255;
+				Green = (iteration / 60) * 255;
+			}
+			else if (iteration <= 120)
+			{
+				Red = 255 - (((iteration - 60) / 60) * 255);
+				Green = 255;
+			}
+			else if (iteration <= 180)
+			{
+				Green = 255;
+				Blue = ((iteration - 120) / 60) * 255;
+			}
+			else if (iteration <= 199)
+			{
+				Green = 255 - (((iteration - 60) / 60) * 255);
+				Blue = 255;
+			}
+			else
+			{
+				Red = 0;
+				Green = 0;
+				Blue = 0;
+			}
 
 			// Write the pixel
-			// TODO: change the right-hand side of these three lines to write the desired pixel colour value
-			image(pixelX, pixelY, 0, 0) = rand(); // red component
-			image(pixelX, pixelY, 0, 1) = rand(); // green component
-			image(pixelX, pixelY, 0, 2) = rand(); // blue component
+			// The right-hand side of these three lines writes the desired pixel colour value
+			image(pixelX, pixelY, 0, 0) = Red; // red component
+			image(pixelX, pixelY, 0, 1) = Green; // green component
+			image(pixelX, pixelY, 0, 2) = Blue; // blue component
 		}
 
-		// Uncomment this line to redisplay the image after each row is generated
-		// Useful if your program is slow and you want to verify that it is actually doing something
 		//display.display(image);
 	}
 
 	// Display the complete image
 	display.display(image);
-
-	// Uncomment this line to save the image to disk
-	//image.save_bmp("mandelbrot.bmp");
+	// Save the image to disk
+	image.save_bmp("mandelbrot.bmp");
 
 	// Wait for the window to be closed
 	while (!display.is_closed())
