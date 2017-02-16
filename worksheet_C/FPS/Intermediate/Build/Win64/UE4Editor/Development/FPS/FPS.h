f82d9334355ec23dd63f0a836f7d3a6dd85caab1
jack-maber <jack-maber@live.co.uk>
2017-02-16 14:38:45 +0000

    Added new template

--- worksheet_C/FPS/Intermediate/Build/Win64/UE4Editor/Development/FPS/FPS.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_FPS_H__
-#define __AUTO_FPS_H__
-//Last Write: 15/02/2017 15:37:35 +00:00
-#include "C:\Users\Jack\Documents\Uni Folder\comp130-worksheets\worksheet_C\FPS\Source\FPS\FPS.h"
-#endif//__AUTO_FPS_H__
--- worksheet_C/FPS/Source/FPS/FPS.h
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
-
-#ifndef __FPS_H__
-#define __FPS_H__
-
-#include "EngineMinimal.h"
-
-
-#endif
