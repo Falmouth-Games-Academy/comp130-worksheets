7c6e77641e397f880a1bf6797c1c290e6e9b674c
TristanBarlow <tristanbarlowg@gmail.com>
2017-02-21 18:03:02 +0000

    Add files via upload

---
+++ worksheet_C/Source/WorkSheetC/MySaveGame.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WorkSheetC.h"
+#include "MySaveGame.h"
+
+
+UMySaveGame::UMySaveGame()
+{
+	SaveSlotName = TEXT("TestSaveSlot");
+	UserIndex = 0;
+}
+
---
+++ worksheet_C/Source/WorkSheetC/WorkSheetC.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "WorkSheetC.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WorkSheetC, "WorkSheetC" );
+ 
